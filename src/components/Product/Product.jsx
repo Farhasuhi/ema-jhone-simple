2f1e7dd2de0e577cd8ce66f1178ebc3fbfd7b60e
Farhasuhi <git config --global user.email suhifarhaislam@gmail.com>
2023-03-28 08:40:37 +0600

    s

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import './Product.css'
+import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 const Product = (props) => {
     const { img, name, price, seller, ratings } = props.product;
+    const {handleAddToCart}=props
     console.log(props)
+    
     return (
         <div className='card'>
             <img src={img ? img : "no-image"} alt="" />
@@ -13,7 +17,7 @@ const Product = (props) => {
                 <p>Manufacturer:{seller}</p>
                 <p>Rating:{ratings}stars</p>
             </div>
-            <button className='btn'>Add to Cart</button>
+            <button onClick={()=>handleAddToCart(props.product)} className='btn'>Add to Cart  <FontAwesomeIcon icon={faCartShopping} /></button>
         </div>
     );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -5,20 +5,27 @@ import Product from '../Product/Product';
 import './Shop.css'
 const Shop = () => {
     const[products,setProducts]=useState([]);
+    const[cart,setCart]=useState([])
     useEffect(()=>{
         fetch('products.json')
         .then(res=>res.json())
         .then(data=>setProducts(data))
     },[])
+    const handleAddToCart=(product)=>{
+        const newCart=[...cart,product];
+        setCart(newCart);
+        
+
+    }
     return (
         <div className='shop-container'>
             <div className='products-container'>
                 {
-                    products.map(product=><Product product={product} key={product.id}></Product>)
+                    products.map(product=><Product product={product} key={product.id}handleAddToCart={handleAddToCart}></Product>)
                 }
             </div>
             <div className='cart-container'>
-                <h4>Order summary</h4>
+                <h4>Order summary:{cart.length}</h4>
             </div>
         </div>
     );
