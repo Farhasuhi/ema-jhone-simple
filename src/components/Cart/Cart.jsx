36374a3cce613b1a731374e60f2e3c6e5fd18533
Farhasuhi <git config --global user.email suhifarhaislam@gmail.com>
2023-03-29 08:27:26 +0600

    final

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,10 +7,12 @@ const Cart = (props) => {
     console.log('cart',cart)
     let total=0;
     let shippingTotal=0;
+    let quantity=0;
     for(let product of cart){
-        total=total+product.price;
+        product.quantity=product.quantity||1;
         shippingTotal=shippingTotal+product.shipping;
-        
+        quantity=quantity+product.quantity;
+        total=total+product.price*quantity;
     }
     const tax=(total*7/100);
     const grandTotal=(total+tax+shippingTotal);
@@ -22,7 +24,7 @@ const Cart = (props) => {
             </p>
             <p>Total-shipping:{shippingTotal}</p>
             <p>Tax:{tax.toFixed(2)}</p>
-            <p>quantity:{cart.length}</p>
+            <p>quantity:{quantity}</p>
             <h6>Grand Total:{grandTotal.toFixed(2)}</h6>
             <button className='btn-clear'>Clear Cart <FontAwesomeIcon icon={faTrash} /></button>
             <button className='btn-review'>Review order <FontAwesomeIcon icon={faArrowRight} /></button>
